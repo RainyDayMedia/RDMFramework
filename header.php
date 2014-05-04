b6609e2bfe9e36217f83796c4e1aa40194076fa8
Ed Heltzel <ed@rainydaymedia.net>
2014-05-04 12:01:16 -0400

    added human txt

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="author" href="/inc/humans.txt" />
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
