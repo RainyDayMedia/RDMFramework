f75019d942a3beaab12a19a7c1ec36516140b79a
Ed Heltzel <ed@rainydaymedia.net>
2014-05-21 08:13:46 -0400

    Added Sassy_s
    integrated sassy_s SCSS setup in place of base CSS.

--- header.php
+++ header.php
@@ -9,14 +9,15 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="author" href="/inc/humans.txt" />
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
 
-<?php wp_head(); ?>
+	<link rel="author" href="/inc/humans.txt" />
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
