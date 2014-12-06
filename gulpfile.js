0374e7182185320f31e9c8fba61a99389a4ad5b7
Todd Miller <toddses@gmail.com>
2014-12-06 11:40:28 -0500

    adds gulp plumber plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
 // Include gulp
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 
 // pull in the plugins
-$ = require('gulp-load-plugins')();
-var browserSync = require('browser-sync');
+$ = require('gulp-load-plugins')({
+            pattern: ['gulp-*', 'browser-sync']
+});
 
 // browser-sync task for starting the server.
 gulp.task('browser-sync', function() {
-    browserSync({
+    $.browserSync({
         proxy: "./"
     });
 });
 
 // task for reloading the browser
 gulp.task('bs-reload', function() {
-    browserSync.reload();
+    $.browserSync.reload();
 });
 
 // Lint Javascript Task
@@ -27,24 +28,26 @@ gulp.task('lint', function() {
 // compile, prefix, and minify the sass
 gulp.task('styles', function() {
     return gulp.src('assets/scss/*.scss')
+        .pipe($.plumber())
         .pipe($.rubySass({ "sourcemap=none": true }))
         .pipe($.autoprefixer(["> 1%", "last 2 versions", "Firefox ESR", "Opera 12.1" , "ie 7"], { cascade: true }))
         .pipe(gulp.dest('.'))
         .pipe($.rename({suffix: '.min'}))
         .pipe($.minifyCss())
         .pipe(gulp.dest('.'))
-        .pipe(browserSync.reload({stream:true}));
+        .pipe($.browserSync.reload({stream:true}));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
     return gulp.src('assets/js/*.js')
+        .pipe($.plumber())
         .pipe($.sourcemaps.init())
             .pipe($.concat('all.min.js'))
             .pipe($.uglify())
         .pipe($.sourcemaps.write('../maps'))
         .pipe(gulp.dest('assets/js/dist'))
-        .pipe(browserSync.reload({stream:true}));
+        .pipe($.browserSync.reload({stream:true}));
 });
 
 // Watch Files For Changes
