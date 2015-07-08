7a70c0b1dc294272c450b14a058ab121ef9643b9
Ed Heltzel <ed@rainydaymedia.net>
2015-07-08 10:29:20 -0400

    updated email from addres and name

--- functions.php
+++ functions.php
@@ -128,3 +128,27 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Update Email Address for WordPress
+ */
+	// AUTO-DETECT THE SERVER
+	function rdm_filter_wp_mail_from($email){
+		// START OF CODE LIFTED FROM WORDPRESS CORE
+		$sitename = strtolower( $_SERVER['SERVER_NAME'] );
+		if ( substr( $sitename, 0, 4 ) == 'www.' ) {
+			$sitename = substr( $sitename, 4 );
+		}
+		// END OF CODE LIFTED FROM WORDPRESS CORE
+		$myfront = "web-master@";
+		$myback = $sitename;
+		$myfrom = $myfront . $myback;
+		return $myfrom;
+	}
+	add_filter("wp_mail_from", "rdm_filter_wp_mail_from");
+
+    // FROM EMAIL NAME
+    function rdm_filter_wp_mail_from_name($from_name){
+        return "Web Master";
+    }
+    add_filter("wp_mail_from_name", "rdm_filter_wp_mail_from_name");
